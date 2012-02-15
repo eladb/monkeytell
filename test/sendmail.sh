f080b039e2a03f116a2af608c0756634f40e2c21
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 19:07:09 +0200

    add mimelib

--- test/sendmail.sh
+++ test/sendmail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash	
 echo sending mail to $MONKEYSERVER
-swaks -h monkeytell.com --from elad.benisrael@gmail.com --to all@monkeytell.com --server $MONKEYSERVER
+swaks -h monkeytell.com --from elad.benisrael@gmail.com --to another@monkeytell.com,nonmonkeytell@gmail.com,all@monkeytell.com --server $MONKEYSERVER
