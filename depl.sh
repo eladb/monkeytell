00f2b09593efb66fa717ccfeaea1e250d5359bd9
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 16:51:15 +0200

    add rsyslog/loggly deployment

--- depl.sh
+++ depl.sh
@@ -4,6 +4,18 @@
     exit 2;
 }
 
+echo 'installing rsyslog/loggly'
+
+# make sure mahcine is in allow-list of loggly
+curl -X POST -u eladb:Gdemst19 http://monkeytell.loggly.com/api/inputs/14401/adddevice
+
+# install rsyslogd config for monkeytell
+sudo cp install/rsyslog.d/* /etc/rsyslog.d
+
+# reload config for rsyslog
+sudo reload rsyslog
+
+
 echo 'pulling new code from git'
 git reset --hard || exit 1
 git pull --rebase origin || exit 1
