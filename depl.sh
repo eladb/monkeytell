bcf68f97c08c7a25f9bcd9b9ffed3e23847287f4
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 12:36:46 +0200

    improve deploy script to recreate upstart link in /etc/init

--- depl.sh
+++ depl.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git reset --hard && git pull --rebase origin && npm install && sudo initctl reload-configuration && sudo start monkeytell
-
+git reset --hard || exit 1
+git pull --rebase origin || exit 1
+[ -e /etc/init/monkeytell.conf ] || sudo ln -s $PWD/monkeytell.conf /etc/init
+sudo initctl reload-configuration
+sudo start monkeytell
+sudo restart monkeytell
