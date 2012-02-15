291d3a4dbbc5647b9d4b4ea8b92646bdd581afca
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 12:53:13 +0200

    add deps port

--- depl.sh
+++ depl.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+[ $PROD ] || {
+    echo 'Running in non-production environment (PROD=0)';
+    exit 2;
+}
+
 git reset --hard || exit 1
 git pull --rebase origin || exit 1
 [ -e /etc/init/monkeytell.conf ] || sudo ln -s $PWD/monkeytell.conf /etc/init
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var HTTP_PORT = process.env.HTTP_PORT || 5080;
 var SMTP_PORT = process.env.SMTP_PORT || 5025;
+var DEPS_PORT = process.env.DEPS_PORT || 5033;
+var spawn = require('child_process').spawn;
+var express = require('express');
 
 var app = http.createServer(function(req, res) {
     res.end('email is back, baby v3');
@@ -16,3 +19,16 @@ smtp.listen(SMTP_PORT);
 
 console.log('Listening on', HTTP_PORT, 'and', SMTP_PORT);
 
+var deps = express.createServer();
+deps.post('/', function(req, res) {
+	var depl = spawn('./depl.sh');
+	res.writeHead(200, {'content-type':'text/plain'});
+	depl.stdout.pipe(res);
+	depl.stderr.pipe(res);
+	depl.on('exit', function(code) {
+		console.log('exit', code);
+		res.end();
+	});
+});
+deps.listen(DEPS_PORT);
+console.log('deps listening on port', DEPS_PORT);
