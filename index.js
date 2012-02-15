8ab71cd5c21202d03704372cf26cad53cf243c1d
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 12:02:05 +0200

    output ports

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var http = require('http');
+var HTTP_PORT = process.env.HTTP_PORT || 5080;
+var SMTP_PORT = process.env.SMTP_PORT || 5025;
 
 var app = http.createServer(function(req, res) {
     res.end('email is back, baby');
 });
 
-app.listen(process.env.HTTP_PORT || 5080);
 
 var smtp = http.createServer(function(req, res) {
     res.end('smtp');
 });
 
-smtp.listen(process.env.SMTP_PORT || 5025);
+app.listen(HTTP_PORT);
+smtp.listen(SMTP_PORT);
+
+console.log('Listening on', HTTP_PORT, 'and', SMTP_PORT);
+
