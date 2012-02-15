649b95bb67fba5f3e718945f90e9ae7e50495b3c
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 12:41:43 +0200

    update deploy

--- depl.sh
+++ depl.sh
@@ -3,5 +3,5 @@ git reset --hard || exit 1
 git pull --rebase origin || exit 1
 [ -e /etc/init/monkeytell.conf ] || sudo ln -s $PWD/monkeytell.conf /etc/init
 sudo initctl reload-configuration
+sudo stop monkeytell
 sudo start monkeytell
-sudo restart monkeytell
