97407b45573e43b8bb78b17959b279fe43d30bca
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-02-15 12:59:04 +0200

    refactor spawn out

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var express = require('express');
+var path = require('path');
+
 var HTTP_PORT = process.env.HTTP_PORT || 5080;
 var SMTP_PORT = process.env.SMTP_PORT || 5025;
 var DEPS_PORT = process.env.DEPS_PORT || 5033;
-var spawn = require('child_process').spawn;
-var express = require('express');
 
 var app = http.createServer(function(req, res) {
     res.end('email is back, baby v3');
@@ -19,16 +20,12 @@ smtp.listen(SMTP_PORT);
 
 console.log('Listening on', HTTP_PORT, 'and', SMTP_PORT);
 
+//
+// start deps server
+//
+
+var spawn = require('./lib/spawn');
 var deps = express.createServer();
-deps.post('/', function(req, res) {
-	var depl = spawn('./depl.sh');
-	res.writeHead(200, {'content-type':'text/plain'});
-	depl.stdout.pipe(res);
-	depl.stderr.pipe(res);
-	depl.on('exit', function(code) {
-		console.log('exit', code);
-		res.end();
-	});
-});
+deps.post('/', spawn(path.join(__dirname, 'depl.sh')));
 deps.listen(DEPS_PORT);
 console.log('deps listening on port', DEPS_PORT);
